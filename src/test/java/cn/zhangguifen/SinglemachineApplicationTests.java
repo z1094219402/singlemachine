16494472e0b0f79b510d12050731142a878579cc
yangken <1094219402@qq.com>
2020-09-17 23:19:56 +0800

    change some code

--- src/test/java/cn/zhangguifen/SinglemachineApplicationTests.java
+++ src/test/java/cn/zhangguifen/SinglemachineApplicationTests.java
@@ -1,91 +1,84 @@
 package cn.zhangguifen;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@Slf4j
+//@Slf4j
 public class SinglemachineApplicationTests {
-
+	
     @Autowired
     RedisTemplate redisTemplate;
 
     int[] array = new int[]{6,1,3,4,12,67,2,5,9,7};
+    
+    private static Logger log = LoggerFactory.getLogger(SinglemachineApplicationTests.class);
 
-	@Test
-	public void contextLoads() {
-        log.error("%dit is wrong..",5);
-	}
+//	@Test
+//	public void contextLoads() {
+//        log.error("%dit is wrong..",5);
+//	}
 
-	@Test
-	public void setRedis(){
-
-        try {
-            /****************String*****************/
-//            String store = redisTemplate.opsForValue().get("bluetime");
-//            if(store==null || store.equals("")) {
-//                redisTemplate.opsForValue().set("bluetime","hello world");
-//                store = redisTemplate.opsForValue().get("bluetime");
-//            }
-
-            /****************Set*****************/
-//            Set<String> result = redisTemplate.opsForSet().members("setthings");
-//            if(result.size()==0){
-//                log.info("ERROR!!!the size is ..[ "+result.size()+" ]");
-//                log.info("ERROR!!!the result is ..[ "+result+" ]");
-//                for(int i = 0; i < 10; i++){
-//                    String name = "mongey_"+i;
-//                    redisTemplate.opsForSet().add("setthings",name);
+//	@Test
+//	public void setRedis(){
+//		try {
+//            /****************String*****************/
+////            String store = redisTemplate.opsForValue().get("bluetime");
+////            if(store==null || store.equals("")) {
+////                redisTemplate.opsForValue().set("bluetime","hello world");
+////                store = redisTemplate.opsForValue().get("bluetime");
+////            }
+//
+//            /****************Set*****************/
+////            Set<String> result = redisTemplate.opsForSet().members("setthings");
+////            if(result.size()==0){
+////                log.info("ERROR!!!the size is ..[ "+result.size()+" ]");
+////                log.info("ERROR!!!the result is ..[ "+result+" ]");
+////                for(int i = 0; i < 10; i++){
+////                    String name = "mongey_"+i;
+////                    redisTemplate.opsForSet().add("setthings",name);
+////                }
+////            }
+////            result = redisTemplate.opsForSet().members("setthings");
+////            log.info("the size is ..[ "+result.size()+" ]");
+////            log.info("the result is ..[ "+result+" ]");
+//            //get factor in range way
+////            String rangString = redisTemplate.opsForSet().randomMember("setthings");
+////            redisTemplate.opsForSet().randomMembers("setthings",3);
+////            Set<String> result = redisTemplate.opsForSet().members("setthings");
+////            log.info("the size is ..[ "+result.size()+" ]");
+////            log.info("the rang string result is ..[ "+rangString+" ]");
+////            //pop factor
+////            redisTemplate.opsForSet().pop("setthings");
+////            result = redisTemplate.opsForSet().members("setthings");
+////            result = redisTemplate.opsForSet().members("setthings");
+////            log.info("after pop:the size is ..[ "+result.size()+" ]");
+////            log.info("after pop:the result is ..[ "+result+" ]");
+//            /****************List*****************/
+////            ListOperations<String,String> list = redisTemplate.opsForList();
+//            if(redisTemplate.opsForList().size("listthingABC")==0){
+//                log.info("list:the size is ..[ "+redisTemplate.opsForList().size("listthingABC")+" ]");
+//                log.info("the result is ..[ "+redisTemplate.opsForList().range("listthingABC",0, -1)+" ]");
+//                for(int i = 0; i < 12; i ++){
+//                    redisTemplate.opsForList().leftPush("listthingABC","sky_"+i);
 //                }
+//
 //            }
-//            result = redisTemplate.opsForSet().members("setthings");
-//            log.info("the size is ..[ "+result.size()+" ]");
-//            log.info("the result is ..[ "+result+" ]");
-            //get factor in range way
-//            String rangString = redisTemplate.opsForSet().randomMember("setthings");
-//            redisTemplate.opsForSet().randomMembers("setthings",3);
-//            Set<String> result = redisTemplate.opsForSet().members("setthings");
-//            log.info("the size is ..[ "+result.size()+" ]");
-//            log.info("the rang string result is ..[ "+rangString+" ]");
-//            //pop factor
-//            redisTemplate.opsForSet().pop("setthings");
-//            result = redisTemplate.opsForSet().members("setthings");
-//            result = redisTemplate.opsForSet().members("setthings");
-//            log.info("after pop:the size is ..[ "+result.size()+" ]");
-//            log.info("after pop:the result is ..[ "+result+" ]");
-            /****************List*****************/
-//            ListOperations<String,String> list = redisTemplate.opsForList();
-            if(redisTemplate.opsForList().size("listthingABC")==0){
-                log.info("list:the size is ..[ "+redisTemplate.opsForList().size("listthingABC")+" ]");
-                log.info("the result is ..[ "+redisTemplate.opsForList().range("listthingABC",0, -1)+" ]");
-                for(int i = 0; i < 12; i ++){
-                    redisTemplate.opsForList().leftPush("listthingABC","sky_"+i);
-                }
-
-            }
-            log.info("list:the size is ..[ "+redisTemplate.opsForList().size("listthingABC")+" ]");
-            log.info("the result is ..[ "+redisTemplate.opsForList().range("listthingABC",0, -1)+" ]");
-
-
-        }catch(Exception e){
-            log.error("it is wrong..");
-        }
-	}
+//            log.info("list:the size is ..[ "+redisTemplate.opsForList().size("listthingABC")+" ]");
+//            log.info("the result is ..[ "+redisTemplate.opsForList().range("listthingABC",0, -1)+" ]");
+//
+//
+//        }catch(Exception e){
+//            log.error("it is wrong..");
+//        }
+//	}
 
     /**
      * 排序算法测试
